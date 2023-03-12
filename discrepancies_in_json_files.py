58b7df9a3f0134ebd3892a5842e8edb2b45d21c4
MobKnight <lion.noir@gmail.com>
2023-03-13 05:47:45 +0900

    2개 경로 비교 기능 (디렉토리 모드), CLI 기능 추가

--- discrepancies_in_json_files.py
+++ discrepancies_in_json_files.py
@@ -1,5 +1,10 @@
 import json
 from collections.abc import MutableMapping
+import os
+from pathlib import Path
+from datetime import datetime
+import argparse
+
 
 def read_json(file):
     with open(file, 'r') as f:
@@ -8,6 +13,7 @@ def read_json(file):
             return data
         except:
             print('Error while reading file {}. You sure you got the right json file?'.format(file))
+
 
 def flatten(data, parent_key='', sep='.'):
     # Nested json 처리
@@ -19,6 +25,7 @@ def flatten(data, parent_key='', sep='.'):
         else:
             items.append((new_key, value))
     return dict(items)
+
 
 def find_discrepancies(data1, data2):
     discrepancies: dict = {}
@@ -45,7 +52,19 @@ def find_discrepancies(data1, data2):
         }
     return discrepancies
 
-def main(file1, file2, output_file):
+
+def get_json_files_from_dir(dir):
+    dic = {}
+    pathlist = Path(dir).rglob('*.json')
+    for path in pathlist:
+        if os.path.isfile(path):
+            path_in_str = str(path)
+            relpath_in_str = str(os.path.relpath(path, dir)) # Relative Path로 찾아오기
+            dic[relpath_in_str] = path_in_str
+    return dic
+
+
+def compare_json_files(file1, file2):
     print('\n---- Comparing {} and {}...'.format(file1, file2))
     data = read_json(file1)
     data1 = flatten(data)
@@ -53,9 +72,77 @@ def main(file1, file2, output_file):
     data2 = flatten(data)
     discrepancies = find_discrepancies(data1, data2)
     print('===> Discrepancies: {}'.format(discrepancies))
+    return discrepancies
+
+
+def write_output(comparison, discrepancies, output_file):
+    result = {
+        comparison: discrepancies
+    }
+    with open(output_file, 'a') as f:
+        f.write(json.dumps(result, indent=4, sort_keys=True))
+        f.write(',\n')
+
+
+def sign_off(output_file):
+    with open(output_file, 'a') as f:
+        f.write('\n\n{"Job Done": "' + datetime.today().strftime("%Y/%m/%d %H:%M:%S") + '"}]')
+
+
+def main(a, b, output_file):
+    mode: str = ''
+    a = a.strip()
+    b = b.strip()
+    print('\n=-=-=-= Finding discrepancies between {} and {}...'.format(a, b))
+    
+    # 주어진 A, B가 파일인지 디렉토리인지 판별하여 모드 설정하기
+    if Path(a).is_file() and Path(b).is_file():
+        mode = 'file'
+    elif Path(a).is_dir() and Path(b).is_dir():
+        mode = 'dir'
+    else:
+        print('\n---- Error: A and B should be equivalent.\n')
+        return None
+
+    # Output 파일 초기화
     with open(output_file, 'w') as f:
-        f.write(json.dumps(discrepancies, indent=4, sort_keys=True))
-    print('\n=-=-=-= Job Done =-=-=-=')
+        f.write('[\n')
+    
+    # 파일 모드인 경우, 비교 1회 수행하고 종료
+    if mode == 'file':
+        print('---- Running in Files Mode...')
+        result = compare_json_files(a, b)
+        write_output(a + ' and ' + b, result, output_file)
+        sign_off(output_file)
+        print('\n=-=-=-= Job Done =-=-=-=\n')
+        return None
+    
+    # 디렉토리 모드인 경우, A 하위 경로의 모든 JSON 파일을 찾아 동일 경로에 동일 파일이 B 경로에도 존재하면 비교.
+    # A 혹은 B에만 존재하는 파일의 경우 아예 비교하지 않음. 따라서 결과에서도 제외됨.
+    if mode == 'dir':
+        print('---- Running in Directories Mode...')
+        a_files = get_json_files_from_dir(a)
+        b_files = get_json_files_from_dir(b)
+        
+        for key in a_files:
+            if key in b_files:
+                result = compare_json_files(a_files[key], b_files[key])
+                write_output(key, result, output_file)
+        sign_off(output_file)
+
+# main(a='./no_tracking/test.json', b='./no_tracking/test2.json', output_file='./no_tracking/output.json')  # 파일단위 비교 예시
+# main(a='./no_tracking/json_test1', b='./no_tracking/json_test2', output_file='./no_tracking/output.json')   # 디렉토리단위 비교 예시
 
 
-main(file1='./no_tracking/test.json', file2='./no_tracking/test2.json', output_file='./no_tracking/output.json')
+def cli():
+    parser = argparse.ArgumentParser(description='Find discrepancies in JSON Files!\nMade with Python 3.10.10 with default libraries only, but will probably run on 3.5+\nJohn Lee, 2023', formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('source', type=str, help='Called A. Can be a file or directory.')
+    parser.add_argument('target', type=str, help='Called B. Should be equivalent to A.')
+    parser.add_argument('-o', '--output', type=str, required=False, default='output.json', help='Output file name (in JSON). Default is ./output.json.')
+
+    args = parser.parse_args()
+
+    main(args.source, args.target, args.output)
+
+
+cli()
